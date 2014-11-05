2dd423ec0fb17915a238b12426fb193a9b1d26e4
js222xt <js222xt@student.lnu.se>
2014-11-05 15:20:10 +0100

    RED
    RED

--- README.md
+++
@@ -1,8 +0,0 @@
-Testing-Assignment1
-===================
-
-Testing course Assignment 1
-
-Collaborators:
-Jonas Sandroos, js222xt GIT: js222xt
-Robert Jarlvik, rj222sdq GIT: rob83ert
--- src/Tests/Constructor.java
+++ src/Tests/Constructor.java
@@ -7,13 +7,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Constructor {
-
+	
 	@Test
 	public void shouldCreateInstance() 
 	{
 		Calculator c = new Calculator();
 		
 		Assert.assertEquals(true,c instanceof Calculator);
+	}
+	
+	@Test
+	public void shouldMultiply()
+	{
+		Calculator c  = new  Calculator();
+		
+		Assert.assertArrayEquals(25, c.multiply(5,5));
 	}
 
 }
