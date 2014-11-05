315a5122ef21dd415912ce584415c2691c2a2b53
js222xt <js222xt@student.lnu.se>
2014-11-05 13:17:38 +0100

    RED
    RED

---
+++ Testing-Assignment1/src/Tests/ConstructorTest.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConstructorTest {
+
+	@Test
+	public void shouldCreateBallInstance() {
+		new Ball();
+	}
+
+}
