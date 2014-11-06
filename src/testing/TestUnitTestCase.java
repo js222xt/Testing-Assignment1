5f0798775d1fa41351ff7c4eb2759dc9663fe715
rob83ert <cyrix83@hotmail.com>
2014-11-06 10:59:36 +0100

    Green
    Green

---
+++ src/testing/TestUnitTestCase.java
@@ -0,0 +1,26 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import calculator.Calculator;
+
+public class TestUnitTestCase {
+
+	private Calculator c;
+
+	@Before
+	public void initiate()
+	{
+		c = new Calculator();
+	}
+	
+	@Test 
+	public void shouldCreateInstance()
+	{
+		assertTrue(c instanceof Calculator);
+	}
+
+}
