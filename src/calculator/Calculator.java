3700e42f3b2dc17f28ec6df4c1ec7f0fe876dab8
js222xt <js222xt@student.lnu.se>
2014-11-19 15:27:58 +0100

    RED
    RED

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -2,6 +2,7 @@ package calculator;
 
 public class Calculator 
 {
+	//js222xt
 	public double multiply(double a, double b){
 		try{
 			double res = a * b;
@@ -17,6 +18,7 @@ public class Calculator
 		}		
 	}
 
+	//js222xt
 	public double sub(double a, double b) {
 		double res = a - b;
 		if(!Double.isInfinite(res)){
@@ -26,11 +28,13 @@ public class Calculator
 			throw new RuntimeException("Double is Infinite");
 		}
 	}
-
+	
+	//js222xt
 	public double recArea(double length, double hight) {
 		return multiply(length, hight);
 	}
 
+	//js222xt
 	public double triC(double a, double b) {
 		double res = Math.sqrt( Math.pow(a, 2) + Math.pow(b, 2) );
 		if(!Double.isInfinite(res)){
--- src/testing/TestUnitTestCase.java
+++ src/testing/TestUnitTestCase.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import static org.mockito.Mockito.*;
 import calculator.Calculator;
+
 
 public class TestUnitTestCase {
 
 	private Calculator c;
-
+	
 	@Before
 	public void initiate()
 	{
@@ -23,6 +24,7 @@ public class TestUnitTestCase {
 		assertTrue(c instanceof Calculator);
 	}
 
+	//js222xt
 	@Test
 	public void shouldMultiply(){
 		double delta = 0;
@@ -31,42 +33,60 @@ public class TestUnitTestCase {
 		assertEquals(0.0, c.multiply(-2.0,0.0), delta);
 	}
 	
+	//js222xt
 	@Test(expected = RuntimeException.class)
 	public void shouldThrowExceptionMultiply(){
 		c.multiply(Double.MAX_VALUE,5.0);
 	}
 	
+	//js222xt
 	@Test
 	public void shouldSubstractPositive(){
 		double delta = 0;
 		assertEquals(10.0, c.sub(15.0,5.0), delta);
 	}
 	
+	//js222xt
 	@Test
 	public void shouldSubstractNegative(){
 		double delta = 0;
 		assertEquals(-10.0, c.sub(0.0,10.0), delta);
 	}
 	
+	//js222xt
 	@Test(expected = RuntimeException.class)
 	public void shouldThrowExceptionSub(){
 		c.sub(Double.MIN_VALUE,Double.NEGATIVE_INFINITY);
 	}
 	
+	//js222xt
 	@Test
 	public void shouldGetRectangleArea(){
 		double delta = 0;
 		assertEquals(25, c.recArea(5.0,5.0), delta);
 	}
 	
+	//js222xt
 	@Test
 	public void shouldGetRectangleC(){
 		double delta = 0.01;
 		assertEquals(5.0, c.triC(3.0,4.0), delta);
 	}
 	
+	//js222xt
 	@Test(expected = RuntimeException.class)
 	public void shouldThrowExceptionTriC(){
 		c.triC(Double.MAX_VALUE,4.0);
+	}
+	
+	//js222xt
+	@Test
+	public void shouldMockSuperCalculation(){
+		cMock.superCalculation(10.0,5.0,40.0,5.0,6.0);
+		Calculator cMock = mock(Calculator.class);
+		verify(cMock).sub(10.0,5.0);
+		verify(cMock).triC(10.0,5.0);
+		verify(cMock).recArea(40.0,5.0);
+		verify(cMock).multiply(5.0,6.0);
 	}
 }
